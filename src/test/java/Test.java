f91ec13ea31a0e9df99c54b970d41bee27d02ad5
Екатерина Чернова <hd106906bqwerty@gmail.com>
2022-08-06 10:17:14 +0300

    Test was modified

--- src/test/java/Test.java
+++ src/test/java/Test.java
@@ -38,11 +38,11 @@ public class Test {
     @org.junit.jupiter.api.Test
     public void myTest() {
         driver.get("http://localhost:9999");
-        driver.findElement(By.cssSelector("span[data-test-id=\"name\"] input")).sendKeys("Роман");
-        driver.findElement(By.cssSelector("span[data-test-id='phone'] input")).sendKeys("+89650714523");
-        driver.findElement(By.className("checkbox__box")).click();
+        driver.findElement(By.cssSelector("[data-test-id=\"name\"] input")).sendKeys("Алексей Орлов-Чесменский");
+        driver.findElement(By.cssSelector("[data-test-id='phone'] input")).sendKeys("+89650714523");
+        driver.findElement(By.cssSelector("[data-test-id=\"agreement\"]")).click();
         driver.findElement(By.className("button_view_extra")).click();
-        String text = driver.findElement(By.className("Success_successBlock__2L3Cw")).getText();
+        String text = driver.findElement(By.cssSelector("[data-test-id=\"order-success\"]")).getText();
         Assertions.assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", text.trim());
     }
 }
